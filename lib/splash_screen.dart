eafd3fd44497af1a2c5bcf8586d6107884d1322c
nguyenvantu <nguyenvantu020794@gmail.com>
2024-01-09 23:04:49 +0700

    commit 1

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -11,47 +11,100 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background_image.png"),
-            fit: BoxFit.cover,
-          ),
+        body: Container(
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        image: DecorationImage(
+          image: AssetImage("assets/images/background_image.png"),
+          fit: BoxFit.cover,
         ),
-        child: Column(
+      ),
+      child: Stack(children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Align(
+              child: Image.asset(
+                "assets/images/logo.png",
+                width: 213.915,
+                height: 76.064,
+              ),
+            ),
+          ],
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Image.asset(
-                    "assets/images/logo.png",
-                    width: 213.915,
-                    height: 76.064,
-                  ),
-                ),
-              ],
+            Align(
+              child: Image.asset(
+                "assets/images/ellipse_27.png",
+                width: 296,
+                height: 296,
+                color: Colors.white,
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Image.asset(
-                    "assets/images/flutter_logo.png",
-                    width: 111,
-                    height: 32,
-                  ),
-                ),
-              ],
-            )
           ],
-        ) /* add child content here */,
-      ),
-    );
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              child: Image.asset(
+                "assets/images/ellipse_28.png",
+                width: 296,
+                height: 296,
+                color: const Color.fromRGBO(17, 220, 232, 0),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 40, bottom: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(1),
+                child: Image.asset(
+                  "assets/images/ellipse_29.png",
+                  width: 288,
+                  height: 288,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     ClipRRect(
+        //       borderRadius: BorderRadius.circular(1),
+        //       child: Image.asset(
+        //         "assets/images/shad.png",
+        //         width: 296,
+        //         height: 296,
+        //         color: Colors.black,
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Align(
+                child: Image.asset(
+                  "assets/images/flutter_logo.png",
+                  width: 111,
+                  height: 32,
+                ),
+              ),
+            ),
+          ],
+        )
+      ]), /* add child content here */
+    ));
   }
 }
